e3828145d573e21e616cd85d7cf712e942799efa
tnngo <tnngou@gmail.com>
2022-06-05 08:30:37 +0800

    feat: Status增加Data字段

--- sts/status.go
+++ sts/status.go
@@ -1,6 +1,7 @@
 package sts
 
 type Status struct {
-	Code    int    `json:"code"`
-	Message string `json:"message"`
+	Code    int         `json:"code"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
 }
