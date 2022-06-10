b40b86cd5ed7834c2a143b7abf0d37ad30ee686b
tnngo <tnngou@gmail.com>
2022-06-11 04:47:08 +0800

    feat: 增加http状态码处理

---
+++ sts/herr/herr.go
@@ -0,0 +1,22 @@
+package herr
+
+import "github.com/tnngo/hammer/sts"
+
+// 200
+func OK() *sts.Status {
+	return &sts.Status{
+		Code: "OK",
+	}
+}
+
+// 204
+func OkNoContent() *sts.Status {
+	return &sts.Status{}
+}
+
+// 401
+func Unauthorized() *sts.Status {
+	return &sts.Status{
+		Code: "SIGN_ERROR",
+	}
+}
--- sts/status.go
+++ sts/status.go
@@ -1,7 +1,33 @@
 package sts
 
+import "github.com/gin-gonic/gin"
+
 type Status struct {
-	Code string      `json:"code"`
-	Msg  string      `json:"msg"`
-	Data interface{} `json:"data"`
+	Code    string      `json:"code"`
+	Msg     string      `json:"msg"`
+	Data    interface{} `json:"data"`
+	Details interface{} `json:"details"`
+}
+
+func (s *Status) SetMsg(msg string) *Status {
+	s.Msg = msg
+	return s
+}
+
+func (s *Status) SetData(data interface{}) *Status {
+	s.Data = data
+	return s
+}
+
+func (s *Status) SetDetails(details interface{}) *Status {
+	s.Details = details
+	return s
+}
+
+func (s *Status) Resp(ctx *gin.Context) {
+	if s.Code != "" {
+		ctx.JSON(200, nil)
+	} else {
+		ctx.JSON(204, s)
+	}
 }
