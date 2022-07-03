6df8b14deaf5760a0ad6f1914c49759c5656ca6b
tnngo <tnngou@gmail.com>
2022-07-03 14:46:18 +0800

    feat: 删除http检测

--- http.go
+++ http.go
@@ -2,8 +2,6 @@ package hammer
 
 import (
 	"fmt"
-	"net/http"
-	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/tnngo/lad"
@@ -19,36 +17,6 @@ func (h *Http) Run() {
 	if h.s == nil {
 		h.s = gin.Default()
 	}
-	h.s.Use(func(ctx *gin.Context) {
-
-	})
-	h.s.GET("/start/test/good/", func(ctx *gin.Context) {
-
-	})
-
-	go func() {
-		for {
-			lad.L().Debug("正在检测http服务...")
-			time.Sleep(1 * time.Microsecond)
-			resp, err := http.Get(fmt.Sprintf("http://localhost:%d/start/test/good/", h.Port))
-			if err != nil {
-				lad.L().Error(err.Error())
-				continue
-			}
-
-			if resp.StatusCode == http.StatusOK {
-				fields := make([]lad.Field, 0)
-
-				if h.Name != "" {
-					fields = append(fields, lad.String("name", h.Name))
-				}
-				fields = append(fields, lad.String("local_addr", fmt.Sprintf("%s:%d", localIP(), h.Port)))
-
-				lad.L().Info("Http server started successfully", fields...)
-				return
-			}
-		}
-	}()
 
 	if err := h.s.Run(fmt.Sprintf(":%d", h.Port)); err != nil {
 		lad.L().Error(err.Error())
